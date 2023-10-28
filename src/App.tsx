7858bf5275affe6cdf55cc8df23bd5c7756de402
Arivan G. Eka Satria <arivan.gede@gmail.com>
2023-10-28 10:55:47 +0800

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
